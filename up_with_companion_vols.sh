ec64876b313dcfe0520a4999812c0dfcdc421b84
Ryann Micua (office) <rmicua@ssd.org>
2018-01-10 14:43:07 +0800

    change out nginx certs dir to be named volume

--- up_with_companion_vols.sh
+++ up_with_companion_vols.sh
@@ -2,7 +2,7 @@
 
 docker run -d -p 80:80 -p 443:443 \
 -v /var/run/docker.sock:/tmp/docker.sock:ro \
--v /home/pogidude/certs:/etc/nginx/certs:ro \
+-v nginx_proxy_certs:/etc/nginx/certs:ro \
 -v nginx_proxy_vhost:/etc/nginx/vhost.d \
 -v nginx_proxy_root:/usr/share/nginx/html \
 --label com.github.jrcs.letsencrypt_nginx_proxy_companion.nginx_proxy=true \
